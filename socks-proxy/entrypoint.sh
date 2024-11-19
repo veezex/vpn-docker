270c5c9d4f681e1c6a46e10410c4e6fd0a30308b
vzxw <vlad@drewl.com>
2024-11-20 00:17:33 +0300

    fix

--- socks-proxy/entrypoint.sh
+++ socks-proxy/entrypoint.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 sed -i "s/\$NET_INTERFACE/$NET_INTERFACE/g" /etc/sockd.conf
 
 # Подготовка конфигурации для пользователей
 echo "${PROXY_USER}:${PROXY_PASSWORD}" > /etc/sockd.passwd
+chmod 600 /etc/sockd.passwd
 
 # Запуск dante-server
 exec sockd -f /etc/sockd.conf
