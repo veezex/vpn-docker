373ee4c3138027769b3475d28e7cd117698e7883
vzxw <vlad@drewl.com>
2024-11-19 20:54:27 +0300

    alpine

--- strongswan-vpn/entrypoint.sh
+++ strongswan-vpn/entrypoint.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-##### Strongswan settings update #####
+# Обновление конфигурации StrongSwan
 sed -i "s/\$VPN_DOMAIN/$VPN_DOMAIN/g" /etc/ipsec.conf
 
-# Configure credentials in ipsec.secrets
+# Настройка учетных данных в ipsec.secrets
 echo ": ECDSA \"/etc/strongswan/certs/server-key.pem\"" > /etc/ipsec.secrets
 echo "$VPN_USERNAME : EAP \"$VPN_PASSWORD\"" >> /etc/ipsec.secrets
-########################################
 
-##### Setup certificates #####
+# Установка сертификатов через Certbot, если их нет
 if [ ! -f /etc/letsencrypt/live/$VPN_DOMAIN/fullchain.pem ]; then
   certbot certonly --standalone --non-interactive --agree-tos --email "$CERTBOT_EMAIL" -d "$VPN_DOMAIN"
 fi
 
-# Set up automatic certificate renewal with cron
+# Настройка автоматического обновления сертификатов через Cron
 echo "0 0 * * * certbot renew --quiet --deploy-hook '/update_certs_and_restart.sh'" | crontab -
 
-# Start cron in the background
-cron
+# Запуск Cron в фоновом режиме
+crond
 
-# Copy certificates to the required directory for StrongSwan
+# Копирование сертификатов для StrongSwan
 cp /etc/letsencrypt/live/$VPN_DOMAIN/fullchain.pem /etc/strongswan/certs/server-cert.pem
 cp /etc/letsencrypt/live/$VPN_DOMAIN/privkey.pem /etc/strongswan/certs/server-key.pem
 cp /etc/letsencrypt/live/$VPN_DOMAIN/chain.pem /etc/ipsec.d/cacerts/ca-cert.pem
 
-# Check permissions on certificates
+# Проверка прав доступа к сертификатам
 chmod 600 /etc/strongswan/certs/server-cert.pem /etc/strongswan/certs/server-key.pem
 chown root:root /etc/strongswan/certs/server-cert.pem /etc/strongswan/certs/server-key.pem
-########################################
 
+# Запуск StrongSwan
 exec ipsec start --nofork
--- strongswan-vpn/update_certs_and_restart.sh
+++ strongswan-vpn/update_certs_and_restart.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Copy certificates to the required directory for StrongSwan
+# Копирование сертификатов для StrongSwan
 cp /etc/letsencrypt/live/$VPN_DOMAIN/fullchain.pem /etc/strongswan/certs/server-cert.pem
 cp /etc/letsencrypt/live/$VPN_DOMAIN/privkey.pem /etc/strongswan/certs/server-key.pem
 cp /etc/letsencrypt/live/$VPN_DOMAIN/chain.pem /etc/ipsec.d/cacerts/ca-cert.pem
 
-# Check permissions on certificates
+# Проверка прав доступа к сертификатам
 chmod 600 /etc/strongswan/certs/server-cert.pem /etc/strongswan/certs/server-key.pem
 chown root:root /etc/strongswan/certs/server-cert.pem /etc/strongswan/certs/server-key.pem
-
-# Restart StrongSwan
-ipsec restart
